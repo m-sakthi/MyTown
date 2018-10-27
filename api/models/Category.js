b9c69a1f6bbe5643e520cc9c9f77f8876273d05b
Sakthivel <msv300@gmail.com>
2018-10-27 19:00:46 +0530

    Initial Commit Feature: User, Auth, Category, Outlet.

---
+++ api/models/Category.js
@@ -0,0 +1,51 @@
+/**
+ * Category.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: { 
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    name: {
+      type: 'string',
+      required: true,
+      unique: true,
+      description: 'Full representation of the Caregory Name',
+      maxLength: 120,
+      example: 'Haircut'
+    },
+
+    overview: {
+      type: 'string',
+      required: true,
+      description: 'Some overview about that category',
+      example: 'Haircut with all styles are available',
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    outlets: {
+      collection: 'outlet',
+      via: 'category',
+      through: 'outletcategory',
+    },
+
+  },
+
+};
+
---
+++ api/models/OutletCategory.js
@@ -0,0 +1,49 @@
+/**
+ * OutletCategory.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    overview: {
+      type: 'string',
+      description: 'Offered amenities can be described',
+      example: 'Haircut will be provided with Hairwash'
+    },
+
+    price: {
+      type: 'number',
+      required: true,
+      description: 'Price for the category offered by outlet',
+      example: 100.50
+    },
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    outlet: {
+      model: 'outlet',
+    },
+
+    category: {
+      model: 'category',
+    },
+
+  },
+
+};
